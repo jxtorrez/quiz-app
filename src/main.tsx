0a51a6176142ebd970d9aee033d8da8c786cde66
jxtorrez <jtorrez@pragmainvest.com.bo>
2025-08-23 23:05:58 -0400

    agregando pagina  404

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import App from './App.tsx';
 
 // Detectar si estamos en producción
 const isProd = import.meta.env.MODE === 'production'
+// Detecta si fue redirigido desde 404.html (cuando GitHub Pages no encontró la ruta)
+const urlParams = new URLSearchParams(window.location.search);
+const redirectedPath = urlParams.get("p");
+if (redirectedPath) {
+  window.history.replaceState(null, "", redirectedPath);
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
