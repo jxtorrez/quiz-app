0fa4e1c8caf2497e305ed7670e45bc0168334956
jxtorrez <jtorrez@pragmainvest.com.bo>
2025-08-23 22:14:13 -0400

    actualizando para github pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
+// Detectar si estamos en producción
+const isProd = import.meta.env.MODE === 'production'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={isProd ? '/quiz-app' : '/'}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
